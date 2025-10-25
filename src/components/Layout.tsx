d3db3646b63993ed0f8668647b251bbafdcbc00e
MounirSagh <mounirsaghfary@gmail.com>
2025-10-25 12:32:11 +0100

    speed vercel

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,8 @@
-"use client";
-
 import type React from "react";
-
 import { AppSidebar } from "./Sidebar";
 import { Navbar } from "./Navbar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -15,7 +12,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Navbar />
         <main className="flex-1 p-6 pt-6 bg-black min-h-screen">
           {children}
-           <SpeedInsights />
+          <SpeedInsights />
         </main>
       </SidebarInset>
     </SidebarProvider>
